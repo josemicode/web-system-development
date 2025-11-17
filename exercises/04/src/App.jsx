cbcec2fd01f48256381a54c6de451a641d008cf1
josemicode <jmiguelojedagutierrez@gmail.com>
2025-11-17 18:50:54 +0100

    I'll continue in a bit

--- exercises/04/src/App.jsx
+++ exercises/04/src/App.jsx
@@ -3,12 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function AvailableProducts(props) {}
+function AvailableProducts(props) {
+  return (
+    <>
+      <p>Hold on</p>
+    </>
+  )
+}
 
-function Cart(props) {}
+function Cart(props) {
+
+  return (
+    <>
+      <h3>Total: </h3>
+    </>
+  )
+}
 
 function App() {
-  products = [
+  const products = [
     {
       "name": "Apple",
       "price": 2
@@ -25,13 +38,26 @@ function App() {
     }
   ]
 
+  [userItems, setUserItems] = useState([]);
+
+  const addItem = (newItem) => {
+    const updatedItems = [...userItems, newItem];
+    setUserItems(updatedItems);
+  }
+
+  const removeItem = (id) => {
+    const updatedItems = `I'll filter this I guess? Based on the ${id} ofc`
+    setUserItems(updatedItems);
+  }
+
   return (
     <>
-      <h1>List of Available Products</h1>
-      <AvailableProducts />
+      <h1>Welcome to our humble shop </h1>
+      <h2>Here's the list of available products</h2>
+      <AvailableProducts products={products}/>
 
       <h2>Your Cart:</h2>
-      <Cart />
+      <Cart items={userItems} handlerAdd={addItem} handlerRemove={removeItem}/>
     </>
   )
 }
