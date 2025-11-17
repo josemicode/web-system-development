1f3df01ad9fc8a7eb2a2673c9eb35294777f1781
josemicode <jmiguelojedagutierrez@gmail.com>
2025-11-17 18:21:51 +0100

    base app for cart stuff

--- exercises/04/src/App.jsx
+++ exercises/04/src/App.jsx
@@ -3,12 +3,35 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function AvailableProducts(props) {}
+
+function Cart(props) {}
+
 function App() {
-  const [count, setCount] = useState(0)
+  products = [
+    {
+      "name": "Apple",
+      "price": 2
+    },
+
+    {
+      "name": "Pen",
+      "price": 3
+    },
+
+    {
+      "name": "Pineapple",
+      "price": 5
+    }
+  ]
 
   return (
     <>
-      
+      <h1>List of Available Products</h1>
+      <AvailableProducts />
+
+      <h2>Your Cart:</h2>
+      <Cart />
     </>
   )
 }
