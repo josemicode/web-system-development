72f7324c5126d4f7135a92ce57298936b2d1c534
josemicode <jmiguelojedagutierrez@gmail.com>
2025-11-28 23:50:10 +0100

    health check!

--- exercises/05/routes/indexRoutes.js
+++ exercises/05/routes/indexRoutes.js
@@ -2,6 +2,13 @@ import { Router } from "express";
 
 const indexRouter = Router();
 
+indexRouter.get("/health", (_, res) => {
+    res.json({
+        status: 'ok',
+        timestamp: new Date().toISOString(),
+    });
+});
+
 indexRouter.get("/", (_, res) => {
     res.send("Hello World!");
 });
