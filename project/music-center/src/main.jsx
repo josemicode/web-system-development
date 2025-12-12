3df24cbd41fa9ea9dbb0fd2056289d5b59edd185
josemicode <jmiguelojedagutierrez@gmail.com>
2025-12-12 11:09:34 +0100

    Import Axios

--- project/music-center/src/main.jsx
+++ project/music-center/src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom' 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 
 import App from './App.jsx'
-import HomePage from './Pages/HomePage.jsx'
-import AddTrackPage from './Pages/AddTrackPage.jsx'
+import HomePage from './pages/HomePage.jsx'
+import AddTrackPage from './pages/AddTrackPage.jsx'
 import AddAlbumPage from './pages/AddAlbumPage.jsx'
 import AddArtistPage from './pages/AddArtistPage.jsx';
 
